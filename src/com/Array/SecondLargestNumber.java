db6a72d9b04712ef732d2a5123204d0fe424a601
Harsh Deep Khatri <harshdeep21316@gmail.com>
2026-09-10 19:04:33 +0530

    Added new programs

--- src/com/Array/SecondLargestNumber.java
+++ src/com/Array/SecondLargestNumber.java
@@ -24,6 +24,8 @@ public class SecondLargestNumber {
 			  max = arr[i] ;
 			}
 			}
+		System.out.println("The Largest Element in an array is : "+ max);
+
 		for(int i =0; i<arr.length; i++) {
 			 if(arr[i]<max && arr[i]> secondlargest) {
 				 secondlargest = arr[i];
